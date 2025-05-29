7e23f72167b8d17bc8dc2ddef7c6e9a98629f3dd
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-05-29 13:55:56 +0300

    [added] interfaces and repositories for:
    Base Repository
    Car Manufacture
    Car Model
    Car
    Component

---
+++ Vehicheck.API/Vehicheck.Database/Repositories/CarManufacturerRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicheck.Database.Context;
+using Vehicheck.Database.Entities;
+using Vehicheck.Database.Repositories.Interfaces;
+
+namespace Vehicheck.Database.Repositories
+{
+    public class CarManufacturerRepository(VehicheckDbContext databaseContext) : BaseRepository<CarManufacturer>(databaseContext), ICarManufacturerRepository
+    {
+        private readonly VehicheckDbContext _context = databaseContext;
+
+        public async Task<CarManufacturer?> GetCarManufacturerId(int manufacturerId)
+        {
+            return await _context.CarManufacturers
+                .Include(cm => cm.Cars)
+                .Include(cm => cm.Models)
+                .Where(cm => cm.DeletedAt == null)
+                .FirstOrDefaultAsync(cm => cm.Id == manufacturerId);
+        }
+
+        public async Task<List<CarManufacturer>> GetAllCarManufacturer()
+        {
+            return await _context.CarManufacturers
+                .Include(cm => cm.Cars)
+                .Include(cm => cm.Models)
+                .Where(cm => cm.DeletedAt == null)
+                .ToListAsync();
+        }
+
+        public async Task<CarManufacturer> AddCarManufacturerAsync(CarManufacturer carManufacturer)
+        {
+            if (carManufacturer == null)
+                throw new ArgumentNullException(nameof(carManufacturer));
+
+            Insert(carManufacturer);
+            await SaveChangesAsync();
+            return carManufacturer;
+        }
+
+        public async Task<CarManufacturer> UpdateCarManufacturerAsync(CarManufacturer carManufacturer)
+        {
+            if (carManufacturer == null)
+                throw new ArgumentNullException(nameof(carManufacturer));
+
+            Update(carManufacturer);
+            await SaveChangesAsync();
+            return carManufacturer;
+        }
+    }
+}
---
+++ Vehicheck.API/Vehicheck.Database/Repositories/Interfaces/IComponentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicheck.Database.Entities;
+
+namespace Vehicheck.Database.Repositories.Interfaces
+{
+    internal interface IComponentRepository : IBaseRepository<Component>
+    {
+        Task<Component?> GetComponentId(int componentId);
+        Task<List<Component>> GetAllComponent();
+        Task<Component> AddComponentAsync(Component component);
+        Task<Component> UpdateComponentAsync(Component component);
+
+    }
+}
